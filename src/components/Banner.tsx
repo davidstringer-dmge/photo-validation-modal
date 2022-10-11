7e6377e1c91f5609742d50e8eb715fdb515d9f15
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2022-10-11 11:43:28 +0100

    Improve Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import classNames from "classnames";
 import classes from "./Banner.module.css";
-
 import infoUrl from "../assets/info.svg";
 
 type BannerProps = {
@@ -11,9 +9,11 @@ type BannerProps = {
 export const Banner = (props: BannerProps) => {
   return (
     <div className={classNames(props.className, classes.container)}>
-      <img className={classes.icon} src={infoUrl} /> Please adjust your photo to
-      match the requirements closely. Press the help button below for
-      guidelines.
+      <img className={classes.icon} src={infoUrl} />
+      <p>
+        Please adjust your photo to match the requirements closely. Press the
+        help button below for guidelines.
+      </p>
     </div>
   );
 };
